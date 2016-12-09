1fa08c3890e97c35bd3c1269f3f64e8c624b23a9
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2016-12-09 14:57:36 -0500

    Added novice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,9 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
 
+function preload() {
+  game.load.image('novice', 'assets/img/novice.png');
+}
+
+function create() {
+  game.add.sprite(0, 0, 'novice');
+}
