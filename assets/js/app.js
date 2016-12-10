2982bec1421b4870e0dc94a6878cea866c72c59d
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2016-12-09 23:51:16 -0500

    Splited sprite

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'pokero', { preload: preload, create: create });
 
 function preload() {
-  game.load.image('novice', 'assets/img/novice.png');
+  game.load.spritesheet('novice', 'assets/img/novice.png', 57.1428, 111.75, 85);
 }
 
 function create() {
